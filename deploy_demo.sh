c7cdc43fb24c28cf4ca0539574005fb2a924b74f
Jeroen Meidam <jeroen.meidam@jumbo.com>
2022-06-29 11:38:18 +0200

    Tidies up the bash scripts a bit

--- deploy_demo.sh
+++ deploy_demo.sh
@@ -1,3 +1,14 @@
-databricks workspace import_dir --profile ilionx --overwrite adb_notebooks/almost_ok /Users/qnhmeidam@outlook.com/datadays/almost_ok
-databricks workspace import_dir --profile ilionx --overwrite adb_notebooks/bad_example /Users/qnhmeidam@outlook.com/datadays/bad_example
-databricks workspace import_dir --profile ilionx --overwrite adb_notebooks/tools /Users/qnhmeidam@outlook.com/datadays/tools
+#!/bin/bash
+
+# First argument is the databricks username
+# Second argument is the databricks-cli profile to use
+USAGE="Usage: $0 [databricks accountname] [databricks-cli profile]"
+
+if [ ! "$1" ] || [ ! "$2" ]; then
+  echo "$USAGE"
+else
+  databricks workspace import_dir --profile "$1" --overwrite adb_notebooks/almost_ok "/Users/$1/datadays/almost_ok"
+  databricks workspace import_dir --profile "$1" --overwrite adb_notebooks/bad_example "/Users/$1/datadays/bad_example"
+  databricks workspace import_dir --profile "$1" --overwrite adb_notebooks/tools "/Users/$1/datadays/tools"
+fi
+
--- deploy_package.sh
+++ deploy_package.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-PROFILE=ilionx
+# First argument is the databricks-cli profile to use
+PROFILE=$1
+USAGE="Usage: $0 [databricks-cli profile]"
 
-if [ -d dist ]; then
-  rm -r dist build
+
+if [ ! "$1" ]; then
+  echo "$USAGE"
+else
+
+  if [ -d dist ]; then
+    rm -r dist build
+  fi
+
+  python setup.py bdist_wheel
+
+  dbfs cp config/cluster_init.sh dbfs:/FileStore/databripy/config/cluster_init.sh --profile "$PROFILE" --overwrite
+  dbfs cp dist/*.whl dbfs:/FileStore/wheels/ --profile "$PROFILE" --overwrite
+
+  # mkdirs does not do anything if the directory already exists
+  databricks workspace mkdirs /projects/project_x --profile "$PROFILE"
+  databricks workspace import -l PYTHON --profile "$PROFILE" --overwrite adb_notebooks/main.py /projects/project_x/main
+
 fi
-
-python setup.py bdist_wheel
-
-dbfs cp config/cluster_init.sh dbfs:/FileStore/databripy/config/cluster_init.sh --profile $PROFILE --overwrite
-dbfs cp dist/*.whl dbfs:/FileStore/wheels/ --profile $PROFILE --overwrite
-
-databricks workspace mkdirs /projects/project_x --profile $PROFILE
-databricks workspace import -l PYTHON --profile $PROFILE --overwrite adb_notebooks/main.py /projects/project_x/main.py
